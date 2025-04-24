ff3dac9125b1dbbc82d0362eac58cedbdcc6734d
Siyabonga <siyabongakhanyile76@gmail.com>
2025-04-24 08:52:51 +0200

    feature-express: turned  project to express project.

--- Repository.js
+++ Repository.js
@@ -1,69 +1,201 @@
 import fs from 'node:fs/promises'
-import { Movie } from './Models.js'
+import { Movie, Series, Song } from './Models.js'
 
 const DB_PATH = './Database.json'
 
+// Check if file exists
 export const checkIfFileExists = async () => {
     try {
         await fs.access(DB_PATH)
         return true
     } catch (error) {
+        const initialData = {
+            movies: [],
+            series: [],
+            songs: []
+        }
+        await fs.writeFile(DB_PATH, JSON.stringify(initialData, null, 2))
         return false
     }
 }
 
-
+// Add movie
 export const addMovie = async (movie) => {
     const data = await getAllMovies()
     data.push(movie)
-    await fs.writeFile(DB_PATH, JSON.stringify(data, null, 2))
+    const allData = await getAllData()
+    allData.movies = data
+    await fs.writeFile(DB_PATH, JSON.stringify(allData, null, 2))
     return movie
 }
 
-
+// Delete movie by name
 export const deleteMovieByName = async (name) => {
     const data = await getAllMovies()
     const filtered = data.filter(m => m.name !== name)
-    await fs.writeFile(DB_PATH, JSON.stringify(filtered, null, 2))
+    const allData = await getAllData()
+    allData.movies = filtered
+    await fs.writeFile(DB_PATH, JSON.stringify(allData, null, 2))
     return filtered
 }
 
-
+// Post movie
 export const postMovie = async (movieData) => {
     const data = await getAllMovies()
-    const newMovie = {...movieData }
+    const newMovie = { ...movieData }
     data.push(newMovie)
-    await fs.writeFile(DB_PATH, JSON.stringify(data, null, 2))
+    const allData = await getAllData()
+    allData.movies = data
+    await fs.writeFile(DB_PATH, JSON.stringify(allData, null, 2))
     return newMovie
 }
 
-
+// Update movie
 export const updateMovie = async (movie) => {
     const data = await getAllMovies()
     const index = data.findIndex(m => m.name === movie.name)
     if (index !== -1) {
         data[index] = movie
-        await fs.writeFile(DB_PATH, JSON.stringify(data, null, 2))
+        const allData = await getAllData()
+        allData.movies = data
+        await fs.writeFile(DB_PATH, JSON.stringify(allData, null, 2))
         return movie
     } else {
         return null
     }
 }
 
-
+// Get all movies
 export const getAllMovies = async () => {
     try {
-        const data = await fs.readFile(DB_PATH, 'utf-8')
-        return JSON.parse(data)
-        
+        const raw = await fs.readFile(DB_PATH, 'utf-8')
+        const data = JSON.parse(raw)
+        return data.movies || []
     } catch (err) {
-        console.log(data)
         return []
     }
 }
 
-
+// Get movie by name
 export const getMovieByName = async (name) => {
     const data = await getAllMovies()
     return data.find(m => m.name === name)
+}
+
+// SERIES
+
+export const getAllSeries = async () => {
+    const data = await getAllData()
+    return data.series || []
+}
+
+export const getSeriesByName = async (name) => {
+    const data = await getAllSeries()
+    return data.find(s => s.name === name)
+}
+
+export const addSeries = async (series) => {
+    const data = await getAllSeries()
+    data.push(series)
+    const allData = await getAllData()
+    allData.series = data
+    await fs.writeFile(DB_PATH, JSON.stringify(allData, null, 2))
+    return series
+}
+
+export const deleteSeriesByName = async (name) => {
+    const data = await getAllSeries()
+    const filtered = data.filter(s => s.name !== name)
+    const allData = await getAllData()
+    allData.series = filtered
+    await fs.writeFile(DB_PATH, JSON.stringify(allData, null, 2))
+    return filtered
+}
+
+export const postSeries = async (seriesData) => {
+    const data = await getAllSeries()
+    const newSeries = { ...seriesData }
+    data.push(newSeries)
+    const allData = await getAllData()
+    allData.series = data
+    await fs.writeFile(DB_PATH, JSON.stringify(allData, null, 2))
+    return newSeries
+}
+
+export const updateSeries = async (series) => {
+    const data = await getAllSeries()
+    const index = data.findIndex(s => s.name === series.name)
+    if (index !== -1) {
+        data[index] = series
+        const allData = await getAllData()
+        allData.series = data
+        await fs.writeFile(DB_PATH, JSON.stringify(allData, null, 2))
+        return series
+    } else {
+        return null
+    }
+}
+
+// SONGS
+
+export const getAllSongs = async () => {
+    const data = await getAllData()
+    return data.songs || []
+}
+
+export const getSongByName = async (name) => {
+    const data = await getAllSongs()
+    return data.find(s => s.name === name)
+}
+
+export const addSong = async (song) => {
+    const data = await getAllSongs()
+    data.push(song)
+    const allData = await getAllData()
+    allData.songs = data
+    await fs.writeFile(DB_PATH, JSON.stringify(allData, null, 2))
+    return song
+}
+
+export const deleteSongByName = async (name) => {
+    const data = await getAllSongs()
+    const filtered = data.filter(s => s.name !== name)
+    const allData = await getAllData()
+    allData.songs = filtered
+    await fs.writeFile(DB_PATH, JSON.stringify(allData, null, 2))
+    return filtered
+}
+
+export const postSong = async (songData) => {
+    const data = await getAllSongs()
+    const newSong = { ...songData }
+    data.push(newSong)
+    const allData = await getAllData()
+    allData.songs = data
+    await fs.writeFile(DB_PATH, JSON.stringify(allData, null, 2))
+    return newSong
+}
+
+export const updateSong = async (song) => {
+    const data = await getAllSongs()
+    const index = data.findIndex(s => s.name === song.name)
+    if (index !== -1) {
+        data[index] = song
+        const allData = await getAllData()
+        allData.songs = data
+        await fs.writeFile(DB_PATH, JSON.stringify(allData, null, 2))
+        return song
+    } else {
+        return null
+    }
+}
+
+// Shared read
+const getAllData = async () => {
+    try {
+        const raw = await fs.readFile(DB_PATH, 'utf-8')
+        return JSON.parse(raw)
+    } catch (err) {
+        return { movies: [], series: [], songs: [] }
+    }
 }
--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 import http from 'node:http';
 import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 import { Movie,Series,Song } from './Models.js';
-import { getAllMovies,getMovieByName,deleteMovieByName,updateMovie,postMovie } from './Repository.js';
+import { getAllMovies,
+    getMovieByName,
+    deleteMovieByName,
+    updateMovie,
+    postMovie,
+    getAllSeries,
+    getSeriesByName,
+    deleteSeriesByName,
+    updateSeries,
+    postSeries,
+    getAllSongs,
+    getSongByName,
+    deleteSongByName,
+    updateSong,
+    postSong,
+    checkIfFileExists} from './Repository.js';
 
 const hostname = "127.0.0.1"
 const port = 3000
 const app = express()
 
 app.use(express.json())
-
-//database
-let seriesList = [new Series("Loki",2,9,13),new Series("She hulk",1,9,13),new Series("Batman",3,9,13)]
-let songsList = [new Song("Versace","Migos","Hip hop",200000,new Song("Taylor Port Junkie","Rylo Rodriguez","Hip hop",1000000))]
 
 //endpoints
 const movies = "/Movies"
@@ -21,24 +38,30 @@ const seriesById = "/Series/:name"
 const songs = "/Songs"
 const songsById = "/Songs/:name"
 
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,"/documentation.html"));
+});
 handleMoviesEndpoint()
+handleSeriesEndpoint()
+handleSongsEndpoint()
 
 function handleMoviesEndpoint(){
     app.get(movies,async (req,res)=>{
         let movieName = req.query.name
-        console.log(movieName)
-        let foundMovie = await getMovieByName(movieName)
-        res.send(JSON.parse(JSON.stringify({foundMovie})))
-    })
-
-    app.get(movies,async (req,res)=>{
-        let movies = await getAllMovies()
-        res.send(JSON.parse(JSON.stringify({movies})))
+        if(movieName){
+            let foundMovie = await getMovieByName(movieName)
+            res.send(JSON.parse(JSON.stringify({foundMovie})))
+        }else{
+            let movies = await getAllMovies()
+            res.send(JSON.parse(JSON.stringify({movies})))
+        }
+        
     })
 
     app.put(movies,async (req,res) => {
         let jsonobject = req.body
-        updateMovie(new Movie(jsonobject.name,jsonobject.releaseDate,jsonobject.starRating,jsonobject.rating))
+        await updateMovie(new Movie(jsonobject.name,jsonobject.releaseDate,jsonobject.starRating,jsonobject.rating))
         let movies = await getAllMovies()
         res.send(JSON.parse(JSON.stringify({movies})))
     })
@@ -58,43 +81,76 @@ function handleMoviesEndpoint(){
     })
 }
 
-function handleSeriesEndpoint(req,res){
-    if(req.method === "GET"){
-        res.statusCode = 200
-        res.setHeader("content-type","application/json")
-        res.end(JSON.stringify({seriesList}))
-    }else if(req.method === "POST" || req.method === "PUT"){
-        res.statusCode = 200
-        res.setHeader("content-type","application/json")
-        seriesList.push(new Series("Prison break","11.11.2004",9,16))
-        res.end(JSON.stringify({seriesList}))
-    }else if(req.method === "DELETE"){
-        res.statusCode = 200
-        res.setHeader("content-type","application/json")
-        seriesList.pop()
-        res.end(JSON.stringify({seriesList}))
-    }
+function handleSeriesEndpoint() {
+    app.get(series, async (req, res) => {
+        let seriesName = req.query.name
+        if (seriesName) {
+            let foundSeries = await getSeriesByName(seriesName)
+            res.send(JSON.parse(JSON.stringify({ foundSeries })))
+        } else {
+            let allSeries = await getAllSeries()
+            res.send(JSON.parse(JSON.stringify({ allSeries })))
+        }
+    })
+
+    app.put(series, async (req, res) => {
+        let json = req.body
+        await updateSeries(new Series(json.name, json.season, json.starRating, json.rating))
+        let allSeries = await getAllSeries()
+        res.send(JSON.parse(JSON.stringify({ allSeries })))
+    })
+
+    app.post(series, async (req, res) => {
+        let json = req.body
+        await postSeries(new Series(json.name, json.season, json.starRating, json.rating))
+        let allSeries = await getAllSeries()
+        res.send(JSON.parse(JSON.stringify({ allSeries })))
+    })
+
+    app.delete(seriesById, async (req, res) => {
+        let name = req.params.name
+        await deleteSeriesByName(name)
+        let allSeries = await getAllSeries()
+        res.send(JSON.parse(JSON.stringify({ allSeries })))
+    })
 }
 
-function handleSongsEndpoint(req,res){
-    if(req.method === "GET"){
-        res.statusCode = 200
-        res.setHeader("content-type","application/json")
-        res.end(JSON.stringify({songsList}))
-    }else if(req.method === "POST" || req.method === "PUT"){
-        res.statusCode = 200
-        res.setHeader("content-type","application/json")
-        songsList.push(new Song("Shake ah","Tyla","Amapiano",3000000))
-        res.end(JSON.stringify({songsList}))
-    }else if(req.method === "DELETE"){
-        res.statusCode = 200
-        res.setHeader("content-type","application/json")
-        songsList.pop()
-        res.end(JSON.stringify({songsList}))
-    }
+function handleSongsEndpoint() {
+    app.get(songs, async (req, res) => {
+        let songName = req.query.name
+        if (songName) {
+            let foundSong = await getSongByName(songName)
+            res.send(JSON.parse(JSON.stringify({ foundSong })))
+        } else {
+            let allSongs = await getAllSongs()
+            res.send(JSON.parse(JSON.stringify({ allSongs })))
+        }
+    })
+
+    app.put(songs, async (req, res) => {
+        let json = req.body
+        await updateSong(new Song(json.name, json.artist, json.genre, json.noOfStreams))
+        let allSongs = await getAllSongs()
+        res.send(JSON.parse(JSON.stringify({ allSongs })))
+    })
+
+    app.post(songs, async (req, res) => {
+        let json = req.body
+        await postSong(new Song(json.name, json.artist, json.genre, json.noOfStreams))
+        let allSongs = await getAllSongs()
+        res.send(JSON.parse(JSON.stringify({ allSongs })))
+    })
+
+    app.delete(songsById, async (req, res) => {
+        let name = req.params.name
+        await deleteSongByName(name)
+        let allSongs = await getAllSongs()
+        res.send(JSON.parse(JSON.stringify({ allSongs })))
+    })
 }
 
 
 app.listen(port,() => {
     console.log("listening!!")
+    checkIfFileExists()
 })
